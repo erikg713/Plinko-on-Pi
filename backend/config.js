b62a20752001b777770904a0722ae998172d93f9
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-20 18:40:37 -0500

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,7 @@
+require("dotenv").config();
 
+module.exports = {
+  PORT: process.env.PORT || 5000,
+  PI_API_KEY: process.env.PI_API_KEY,  // From Pi Developer Portal
+  PI_API_URL: "https://api.minepi.com/v2"
+};
