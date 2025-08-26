dc403f62773791e32a5e653ba9e88e4db7b86cf7
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-26 18:15:25 -0500

    Update Bet.js

--- backend/models/Bet.js
+++ backend/models/Bet.js
@@ -1,1 +1,16 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
+const betSchema = new Schema(
+  {
+    user: { type: String, required: true }, // pi_uid or user id
+    betAmount: { type: Number, required: true, min: 0 },
+    multiplier: { type: Number, required: true, min: 0 },
+    winnings: { type: Number, default: 0 },
+    txid: { type: String, required: true },
+    paymentId: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Bet", betSchema);
