a0b0b6171df714e48915c903f995e5d1f832e277
Dev713 <53387865+erikg713@users.noreply.github.com>
2026-08-03 15:08:17 -0500

    Update leaderboard_view.sql

--- db/views/leaderboard_view.sql
+++ db/views/leaderboard_view.sql
@@ -1,1 +1,26 @@
+CREATE OR REPLACE VIEW leaderboard_view AS
 
+SELECT
+
+player_uid,
+
+pi_username,
+
+SUM(total_profit) AS profit,
+
+SUM(total_won) AS winnings,
+
+SUM(games_played) AS games
+
+FROM leaderboards l
+
+JOIN players p
+
+USING(player_uid)
+
+GROUP BY
+
+player_uid,
+pi_username
+
+ORDER BY profit DESC;
