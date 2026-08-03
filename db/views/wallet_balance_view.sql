73e5b3568b9204e13ddd27f43a36b7ae0127923f
Dev713 <53387865+erikg713@users.noreply.github.com>
2026-08-03 15:08:39 -0500

    Create wallet_balance_view to summarize player balances

--- db/views/wallet_balance_view.sql
+++ db/views/wallet_balance_view.sql
@@ -1,1 +1,23 @@
+CREATE OR REPLACE VIEW wallet_balance_view AS
 
+SELECT
+
+w.player_uid,
+
+p.pi_username,
+
+w.available_balance,
+
+w.locked_balance,
+
+w.total_deposited,
+
+w.total_withdrawn,
+
+w.lifetime_profit
+
+FROM wallets w
+
+JOIN players p
+
+ON p.player_uid=w.player_uid;
