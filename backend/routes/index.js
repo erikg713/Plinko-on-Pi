fec3f72b48aeecdb963ada4fb5eda59b224bda1f
Dev713 <53387865+erikg713@users.noreply.github.com>
2026-08-17 08:08:52 -0500

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,1 +1,47 @@
+"use strict";
 
+const express = require("express");
+
+const router = express.Router();
+
+router.use(
+    "/admin",
+    require("./admin")
+);
+
+router.use(
+    "/bets",
+    require("./bets")
+);
+
+router.use(
+    "/leaderboard",
+    require("./leaderboard")
+);
+
+router.use(
+    "/users",
+    require("./users")
+);
+
+router.use(
+    "/auth",
+    require("./auth")
+);
+
+router.use(
+    "/health",
+    require("./health")
+);
+
+router.use(
+    "/wallet",
+    require("./wallet")
+);
+
+router.use(
+    "/provably-fair",
+    require("./provablyFair")
+);
+
+module.exports = router;
